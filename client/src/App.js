feefa0572d7c43119054c3596065e67142492689
Said <saidgaffor.sabyrov@nu.edu.kz>
2020-10-18 11:28:24 +0600

    Second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">Service 2</Link>
+            </li>
+            <li>
+              <Link to="/users">Service 1</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+
+          <Route path="/">
+          </Route>
+
+          <Route path="/">
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
