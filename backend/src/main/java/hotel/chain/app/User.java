97e0f8d82f0db6903cc99c387eddce9609712dfb
Assanali Abu <assanali.abu@nu.edu.kz>
2020-10-23 00:23:20 +0600

    Added Olzhas's files

--- backend/src/main/java/hotel/chain/app/User.java
+++ backend/src/main/java/hotel/chain/app/User.java
@@ -1,14 +1,47 @@
-package hotel.chain.app;
+package examplejaxrs;
 
 public abstract class User {
-    private String firstName;
-    private String lastName;
-
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-
-    public void setLastName(String lastName) { this.lastName = lastName; }
-
-    public String getFirstName() { return firstName; }
-
-    public String getLastName() { return lastName; }
+    public String firstname;
+    public String lastname;
+    public String login;
+    public String password;
+    public String id_type;
+    public String id_number;
+    public String address;
+    public String mobile_phone;
+    public String home_phone;
+    public String category;
+    
+    public User(String firstname, String lastname, String login, String password, String id_type,
+                String id_number, String address, String mobile_phone, String home_phone, String category){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.login = login;
+        this.password = password;
+        this.id_type = id_type;
+        this.id_number = id_number;
+        this.address = address;
+        this.mobile_phone = mobile_phone;
+        this.home_phone = home_phone;
+        this.category = category;
+    }
+    
+    public User(User user){
+        this.firstname = user.firstname;
+        this.lastname = user.lastname;
+        this.login = user.login;
+        this.password = user.password;
+        this.id_type = user.id_type;
+        this.id_number = user.id_number;
+        this.address = user.address;
+        this.mobile_phone = user.mobile_phone;
+        this.home_phone = user.home_phone;
+        this.category = user.category;
+    }
+    
+    @Override
+    public String toString() {
+        return firstname + lastname + login + password + id_type
+                + id_number + address + mobile_phone + home_phone + category;
+    }
 }
